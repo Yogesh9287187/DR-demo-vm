0a6642ba00047ff0221d18f68db3001a73b2c009
Yogesh9287187 <yogesh.gorade@outlook.com>
2025-01-27 18:52:25 +0530

    module upgrade

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,4 +1,46 @@
+# Create the network interface
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.vm_name}-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Use the ID of the network interface in the VM resource
 resource "azurerm_windows_virtual_machine" "vm" {
+  name                  = var.vm_name
+  resource_group_name   = var.resource_group_name
+  location              = var.location
+  size                  = var.vm_size
+  admin_username        = var.admin_username
+  admin_password        = var.admin_password
+
+  # Use a dynamic dependency on the NIC ID
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
+
+
+
+
+
+/* resource "azurerm_windows_virtual_machine" "vm" {
   name                  = var.vm_name
   resource_group_name   = var.resource_group_name
   location              = var.location
@@ -30,4 +72,4 @@ resource "azurerm_network_interface" "nic" {
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
   }
-} 
+} */
--- variables.tf
+++ variables.tf
@@ -1,16 +1,20 @@
 variable "vms" {
+  description = "Map of VMs with their configuration"
   type = map(object({
     vm_size       = string
     admin_username = string
+    admin_password = string
   }))
   default = {
     "vm1" = {
       vm_size       = "Standard_B1ls"
       admin_username = "azureuser"
+      admin_password = "P@ssw0rd123!"
     }
     "vm2" = {
       vm_size       = "Standard_B1ls"
       admin_username = "azureadmin"
+      admin_password = "P@ssw0rd456!"
     }
   }
 }
